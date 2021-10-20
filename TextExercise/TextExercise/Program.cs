4d6b0314c596b149ae7ec739ae9f48eb0c81455f
Łukasz Zawadzki <lukasz.zawadzki@smeo.pl>
2021-10-20 09:40:43 +0200

    tekst w console print line

--- TextExercise/TextExercise/Program.cs
+++ TextExercise/TextExercise/Program.cs
@@ -6,7 +6,7 @@ namespace TextExercise
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World branch!");
         }
     }
 }
